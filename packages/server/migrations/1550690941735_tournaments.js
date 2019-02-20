7b8362552007d1a0a06cfb0eff658db0373a9321
VPaulger <vaughn_paulger@live.ca>
2019-02-20 16:25:55 -0500

    feature: new tournament table and rob made changes to drop schema

---
+++ packages/server/migrations/1550690941735_tournaments.js
@@ -0,0 +1,13 @@
+exports.up = pgm => {
+  pgm.sql(`
+    CREATE TABLE "foostown"."tournaments" (
+      "id" SERIAL PRIMARY KEY,
+      "tournament_name" TEXT NOT NULL,
+      "organization_id" INTEGER NOT NULL,
+      "start_date" DATE NOT NULL,
+      "end_date" DATE NOT NULL,
+      "status" TEXT NOT NULL,
+      UNIQUE (tournament_name, organization_id)
+    );
+  `)
+};
