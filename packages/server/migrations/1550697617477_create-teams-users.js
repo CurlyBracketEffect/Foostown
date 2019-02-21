ce4f2697c617555c2e765f45a92cd8082551071d
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-21 14:02:50 -0500

    chore: created a migration file that will create the teams-users table in the database

--- packages/server/migrations/1550697617477_create-teams-users.js
+++ packages/server/migrations/1550697617477_create-teams-users.js
@@ -1,11 +1,8 @@
 exports.up = pgm => {
   pgm.sql(`
-    CREATE TABLE "foostown"."organizations_users" (
-      "user_id" INTEGER REFERENCES users (id),
-      "team_id" INTEGER
+    CREATE TABLE "foostown"."teams_users" (
+      "user_id" INTEGER REFERENCES users (id) NOT NULL,
+      "team_id" INTEGER INTEGER REFERENCES teams (id) NOT NULL
     );
   `)
 };
-
-
-//todo: "team_id" INTEGER REFERENCES teams (id)
