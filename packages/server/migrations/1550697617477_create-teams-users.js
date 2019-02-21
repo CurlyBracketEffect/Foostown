dd18e430eba2f42e59beaa971cd9359b35831153
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-21 14:20:04 -0500

    chore: added the Foreign Key constraint and fixed a typo

--- packages/server/migrations/1550697617477_create-teams-users.js
+++ packages/server/migrations/1550697617477_create-teams-users.js
@@ -1,8 +1,8 @@
 exports.up = pgm => {
   pgm.sql(`
     CREATE TABLE "foostown"."teams_users" (
-      "user_id" INTEGER REFERENCES users (id) NOT NULL,
-      "team_id" INTEGER INTEGER REFERENCES teams (id) NOT NULL
+      "user_id" INTEGER FOREIGN KEY REFERENCES users (id) NOT NULL,
+      "team_id" INTEGER FOREIGN KEY REFERENCES teams (id) NOT NULL
     );
   `)
 };
