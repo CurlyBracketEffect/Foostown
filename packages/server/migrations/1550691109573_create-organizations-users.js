5cab0db5d045246a6bdef6ed2de7cf5797c8f0cb
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-21 14:22:43 -0500

    chore: added the Foreign Key constraint

--- packages/server/migrations/1550691109573_create-organizations-users.js
+++ packages/server/migrations/1550691109573_create-organizations-users.js
@@ -1,8 +1,8 @@
 exports.up = pgm => {
   pgm.sql(`
     CREATE TABLE "foostown"."organizations_users" (
-      "organization_id" INTEGER REFERENCES organizations (id) NOT NULL,
-      "user_id" INTEGER REFERENCES users (id) NOT NULL,
+      "organization_id" INTEGER FOREIGN KEY REFERENCES organizations (id) NOT NULL,
+      "user_id" INTEGER FOREIGN KEY REFERENCES users (id) NOT NULL,
       "is_admin" BOOLEAN NOT NULL
     );
   `)
