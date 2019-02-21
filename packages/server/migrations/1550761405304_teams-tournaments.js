f4a7dddbc38491558adff4feca904b581b7c6b46
VPaulger <vaughn_paulger@live.ca>
2019-02-21 10:18:30 -0500

    new teams_tournaments table with comments

--- packages/server/migrations/1550761405304_teams-tournaments.js
+++ packages/server/migrations/1550761405304_teams-tournaments.js
@@ -3,7 +3,10 @@ exports.up = pgm => {
     CREATE TABLE "foostown"."teams_tournaments" (
       "tournament_id" INTEGER NOT NULL,
       "team_id" INTEGER NOT NULL,
-      "points" INTEGER NOT NULL
+      "points" INTEGER NOT NULL,
     );
   `)
 };
+
+// FOREIGN KEY (tournament_id) REFERENCES tournaments (id),
+// FOREIGN KEY (team_id) REFERENCES teams (id)
