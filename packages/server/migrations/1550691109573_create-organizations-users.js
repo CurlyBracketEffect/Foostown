1fa83482a568c874e3f33da09e01f79eb6d91a11
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-21 11:56:48 -0500

    chore: created a migration file that will create the organizations-users table in the database

--- packages/server/migrations/1550691109573_create-organizations-users.js
+++ packages/server/migrations/1550691109573_create-organizations-users.js
@@ -1,12 +1,9 @@
 exports.up = pgm => {
   pgm.sql(`
     CREATE TABLE "foostown"."organizations_users" (
-      "organization_id" INTEGER,
-      "user_id" INTEGER REFERENCES users (id),
-      "is_admin" BOOLEAN
+      "organization_id" INTEGER REFERENCES organizations (id) NOT NULL,
+      "user_id" INTEGER REFERENCES users (id) NOT NULL,
+      "is_admin" BOOLEAN NOT NULL
     );
   `)
 };
-
-
-//todo: "organization_id" INTEGER REFERENCES organizations (id)
