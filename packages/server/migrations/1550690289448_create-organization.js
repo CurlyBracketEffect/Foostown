806a6feae151ab880ef3d1ca7e9a54aa98d161e8
navihothi <hothi.navi@gmail.com>
2019-02-21 10:34:20 -0500

    fix: added NOT NULL to owner_id

--- packages/server/migrations/1550690289448_create-organization.js
+++ packages/server/migrations/1550690289448_create-organization.js
@@ -4,7 +4,7 @@ exports.up = pgm => {
       "id" SERIAL PRIMARY key,
       "name" text NOT NULL,
       "is_active" boolean NOT NULL,
-      "owner_id" integer REFERENCES users(id)
+      "owner_id" integer REFERENCES users(id) NOT NULL
     );
   `)
 };
