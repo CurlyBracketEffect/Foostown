c595d0b4a541195cfb7887467105b9f7c8a64e5e
VPaulger <vaughn_paulger@live.ca>
2019-02-22 23:15:24 -0500

    feature: created select opponent component

--- packages/client/src/play-game/SelectOpponent.js
+++ packages/client/src/play-game/SelectOpponent.js
@@ -11,12 +11,7 @@ const SelectOpponent = () => (
     </InputLabel>
     <Select
       style={{width: '300px'}}
-      // value={state.age}
-      // onChange={handleChange}
-      // input={<Input name="age" id="age-label-placeholder" />}
-      // displayEmpty
       name="Select Opponent"
-      // className={classes.selectEmpty}
     >
       <MenuItem value="">Player 1</MenuItem>
       <MenuItem value="">Player 2</MenuItem>
