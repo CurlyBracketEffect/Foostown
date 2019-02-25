a0e1819ab299b8d9ac3a393ac238414b9299527f
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-25 12:05:27 -0500

    chore: created a script that will seed users, teams, organizations, teams_users, and organizations_teams with dummy data

--- packages/server/seeds/index.js
+++ packages/server/seeds/index.js
@@ -1,100 +1,227 @@
-const { Pool } = require('pg')
-const squel = require("squel");
-const config = require('../config/default.json')
+const { Pool } = require("pg");
+const squel = require("squel").useFlavour("postgres");
+const config = require("../config/default.json");
 
 const userSeeds = [
   {
-    full_name: 'Rob Gilson',
-    email: 'rob@rob.rob',
-    password: '1234567',
+    fullname: "Rob Gilson",
+    email: "rob@rob.rob",
+    password: "123456"
   },
   {
-    full_name: 'Jorrin Bruns',
-    email: 'Jorrin@Jorrin.com',
-    password: '1234567',
+    fullname: "Jorrin Bruns",
+    email: "Jorrin@Jorrin.com",
+    password: "123456"
   },
   {
-    full_name: 'Eirian Ta',
-    email: 'Eirian@Eirian.com',
-    password: '1234567',
+    fullname: "Eirian Ta",
+    email: "Eirian@Eirian.com",
+    password: "123456"
   },
   {
-    full_name: 'Akshay Manchand',
-    email: 'Akshay@Akshay.com',
-    password: '1234567',
+    fullname: "Akshay Manchand",
+    email: "Akshay@Akshay.com",
+    password: "123456"
   },
   {
-    full_name: 'Navi Hothi',
-    email: 'navi@navi.com',
-    password: '1234567',
+    fullname: "Navi Hothi",
+    email: "navi@navi.com",
+    password: "123456"
   },
   {
-    full_name: 'Vaughn Paulger',
-    email: 'vaughn@vaughn.com',
-    password: '1234567',
-  },
-]
+    fullname: "Vaughn Paulger",
+    email: "vaughn@vaughn.com",
+    password: "123456"
+  }
+];
 
 const teamSeeds = [
   {
-    team_name: 'Rob Gilson',
-    organization_id: 'RED Academy'
+    team_name: "Rob Gilson",
+    organization_id: 1
   },
   {
-    team_name: 'Jorrin Bruns',
-    organization_id: 'RED Academy'
+    team_name: "Jorrin Bruns",
+    organization_id: 1
   },
   {
-    team_name: 'Eirian Ta',
-    organization_id: 'RED Academy'
+    team_name: "Eirian Ta",
+    organization_id: 1
   },
   {
-    team_name: 'Akshay Manchand',
-    organization_id: 'RED Academy'
+    team_name: "Akshay Manchand",
+    organization_id: 1
   },
   {
-    team_name: 'Navi Hothi',
-    organization_id: 'RED Academy'
+    team_name: "Navi Hothi",
+    organization_id: 1
   },
   {
-    team_name: 'Vaughn Paulger',
-    organization_id: 'RED Academy'
+    team_name: "Vaughn Paulger",
+    organization_id: 1
+  }
+];
+
+const teamsUsersSeeds = [
+  {
+    user_id: 1,
+    team_id: 1
   },
-]
+  {
+    user_id: 2,
+    team_id: 2
+  },
+  {
+    user_id: 3,
+    team_id: 3
+  },
+  {
+    user_id: 4,
+    team_id: 4
+  },
+  {
+    user_id: 5,
+    team_id: 5
+  },
+  {
+    user_id: 6,
+    team_id: 6
+  }
+];
+
+const orgsUsersSeeds = [
+  {
+    organization_id: 1,
+    user_id: 1,
+    is_admin: true
+  },
+  {
+    organization_id: 1,
+    user_id: 2,
+    is_admin: true
+  },
+  {
+    organization_id: 1,
+    user_id: 3,
+    is_admin: true
+  },
+  {
+    organization_id: 1,
+    user_id: 4,
+    is_admin: true
+  },
+  {
+    organization_id: 1,
+    user_id: 5,
+    is_admin: true
+  },
+  {
+    organization_id: 1,
+    user_id: 6,
+    is_admin: true
+  }
+];
+
+const orgSeed = {
+  name: "RED Academy",
+  owner_id: 1,
+  is_active: true
+};
 
 const seed = async () => {
+  const pg = await new Pool(config.db).connect();
 
-  const pg = new Pool(config.db)
-  await pg.connect()
+  try {
+    await pg.query('BEGIN')
 
+    console.log("Seeding Users...")
 
-  // await Promise.all(userSeeds.map((userSeed) => (
-  //   pg.query({
-  //     text: 'INSERT INTO users (full_name, email, password) VALUES ($1, $2, $3)',
-  //     values: [userSeed.full_name, userSeed.email, userSeed.password],
-  //   })
-  // )))
+    await Promise.all(
+      userSeeds.map(
+        userSeed =>
+          console.log(
+            squel
+              .insert()
+              .into("foostown.users")
+              .setFields(userSeed)
+              .toParam()
+          ) ||
+          pg.query(
+            squel
+              .insert()
+              .into("foostown.users")
+              .setFields(userSeed)
+              .toParam()
+          )
+      )
+    );
 
-  const usersPromise =  Promise.all(userSeeds.map((userSeed) => (
-    pg.query(
-      squel.insert()
-        .into('users')
-        .setFields(userSeed)
-        .toParams()
-    )
-  )))
+    console.log("Seeding Users... [DONE]")
 
-  const teamsPromise =  Promise.all(teamSeeds.map((teamSeed) => (
-    pg.query(
-      squel.insert()
-        .into('teams')
-        .setFields(teamSeed)
-        .toParams()
-    )
-  )))
+    const orgPromise = pg.query(
+      squel
+        .insert()
+        .into("foostown.organizations")
+        .setFields(orgSeed)
+        .toParam()
+    );
 
-  await Promise.all([usersPromise, teamsPromise])
+    const teamsPromise = Promise.all(
+      teamSeeds.map(teamSeed =>
+        pg.query(
+          squel
+            .insert()
+            .into("foostown.teams")
+            .setFields(teamSeed)
+            .toParam()
+        )
+      )
+    );
+
+    await Promise.all([orgPromise, teamsPromise])
+
+    const teamsUsersPromise = Promise.all(
+      teamsUsersSeeds.map(teamsUsersSeed =>
+        pg.query(
+          squel
+            .insert()
+            .into("foostown.teams_users")
+            .setFields(teamsUsersSeed)
+            .toParam()
+        )
+      )
+    );
+
+    const orgsUsersPromise = Promise.all(
+      orgsUsersSeeds.map(orgsUsersSeed =>
+        pg.query(
+          squel
+            .insert()
+            .into("foostown.organizations_users")
+            .setFields(orgsUsersSeed)
+            .toParam()
+        )
+      )
+    );
+
+    await Promise.all([teamsUsersPromise, orgsUsersPromise])
+    await pg.query('COMMIT')
+  } catch (e) {
+    await pg.query('ROLLBACK')
+    throw e
+  } finally {
+    pg.release();
+  }
 }
+// await Promise.all(userSeeds.map((userSeed) => (
+//   pg.query({
+//     text: 'INSERT INTO users (full_name, email, password) VALUES ($1, $2, $3)',
+//     values: [userSeed.full_name, userSeed.email, userSeed.password],
+//   })
+// )))
 
-seed();
-
+seed().catch(e => {
+  setImmediate(() => {
+    throw e;
+  });
+})
