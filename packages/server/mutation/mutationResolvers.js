04e40e07b2c6f271df1104b46aab2ca4c644dfc0
Eirian-Ta <trang.ta911@gmail.com>
2019-02-25 13:15:13 -0500

    feat: signup function without authentication

--- packages/server/mutation/mutationResolvers.js
+++ packages/server/mutation/mutationResolvers.js
@@ -1,9 +1,28 @@
-module.exports = () => {
-  return {
+module.exports = {
     Mutation: {
-      async user(parent, args, { req, app }) {
-        return "Success"
+      async signup(parent, args, { req, app, postgres }) {
+        console.log("Test");
+        console.log(args);  
+        const newUserInsert = {
+          text: 'INSERT INTO foostown.users (fullname, email, password) VALUES ($1, $2, $3) RETURNING *', 
+          values: [args.input.fullname, args.input.email, args.input.password],
+        }
+      try {
+        const user = await postgres.query(newUserInsert);
+        console.log(user);
+        return user.rows[0];
+      } catch (e) {
+        switch (true) {
+          case /users_fullname_key/.test(e.message):
+            throw 'An account with this username already exists.';
+          case /users_email_key/.test(e.message):
+            throw 'An account with this email already exists.';
+          default:
+            throw new Error(e);
+        }
       }
-    }
+    },
+
+
+      }
   }
-}
--- packages/server/schema.js
+++ packages/server/schema.js
@@ -1,7 +1,47 @@
 const { gql } = require('apollo-server-express')
 
 module.exports = gql`
+  
+  type User {
+    id: ID!
+    fullname: String
+    email: String!
+    password: String!
+  }
+
+  type Team {
+    id: ID!
+    organization_id: ID!
+    team_name: String!
+  }
+
+  type Match {
+    id: ID!
+    team_id: ID!
+    goals_for: Int!
+    goals_against: Int!
+  }
+
+  input NewUserInput {
+    fullname: String
+    email: String!
+    password: String!
+  }
+
+  input NewMatchInput {
+    id: ID!
+    team_id: ID!
+    goals_for: Int!
+    goals_against: Int!
+  }
+
   type Query {
     user: String!
   }
+
+  type Mutation {
+    signup(input: NewUserInput!): User!
+    createTeam(input: ID!): Team!
+    createMatch(input: NewMatchInput): Match!
+  } 
 `
