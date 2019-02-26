6022e46bfa74c147e13793ab007c2a567106a9e0
VPaulger <vaughn_paulger@live.ca>
2019-02-26 10:51:55 -0500

    style: added border to text fields

--- packages/client/src/play-game/CreateGamePage.js
+++ packages/client/src/play-game/CreateGamePage.js
@@ -7,6 +7,10 @@ import { Link } from 'react-router-dom'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 
+//apollo
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
 //material-ui
 import { 
   Typography, FormControl, TextField, Button 
@@ -24,117 +28,153 @@ const CreateGamePage = () => (
       alignItems: 'center',
     }}
   >
-    <Typography style={{ marginTop: 25 }} variant='overline'>Create Game</Typography>
-    <Link style={{ marginTop: 25 }} to='/home'><button>Back To Home</button></Link>
 
-    <Formik
-      initialValues={{
-        selectOpponent: [],
-        yourScore: '',
-        opponentScore: '',
+    <Typography style={{ marginTop: 25, marginBottom: 25 }} variant='overline'>Create Game</Typography>
+
+    <Link style={{ marginBottom: 25, textDecoration: 'none', width: '50%' }} to='/home'>
+      <Button 
+        style={{
+          width: '100%'
+        }}
+        color='primary'
+        variant='contained'
+      >
+        Back To Home
+      </Button>
+    </Link>
+    {/* <Mutation
+      onError={(error) => {
+        alert(error)
       }}
-      onSubmit={(values, { setSubmitting }) => {
-        values = {
-          ...values,
-          selectOpponent: values.selectOpponent.map(opponent => opponent.value),
-        }
-        // createGame({ variables: {
-        //   insert key here: insert value here
-        // } })
-        setSubmitting(false)
-      }}
-      validationSchema={Yup.object().shape({
-        selectOpponent: Yup.array()
-          .of(
-            Yup.object().shape({
-              // insert key here: Yup.string().required(),
-            })
-          ),
-        yourScore: Yup.string()
-          .required('required'),
-        opponentScore: Yup.string()
-          .required('required'),
-      })}
+      mutation={gql`
+        mutation($item: NewItemInput!) {
+          addItem (
+            input: $item	
+          ) {
+              id
+              title
+              description
+            }
+        }  
+      `}
     >
-      {props => {
-        const {
-          handleSubmit,
-          isSubmitting,                 
-        } = props
-        return (
-          <form
-            onSubmit={handleSubmit}
-            style={{
-              marginTop: 50,
-              width: '300px',
-              display: 'flex', 
-              flexDirection: 'column',
-              alignItems: 'center'
+      {(createGame, { loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+
+        return ( */}
+          <Formik
+            initialValues={{
+              selectOpponent: [],
+              yourScore: '',
+              opponentScore: '',
             }}
+            onSubmit={(values, { setSubmitting }) => {
+              values = {
+                ...values,
+                selectOpponent: values.selectOpponent.map(opponent => opponent.value),
+              }
+              // createGame({ variables: {
+              //   insert key here: insert value here
+              // } })
+              setSubmitting(false)
+            }}
+            validationSchema={Yup.object().shape({
+              selectOpponent: Yup.array()
+                .of(
+                  Yup.object().shape({
+                    // insert key here: Yup.string().required(),
+                  })
+                ),
+              yourScore: Yup.string()
+                .required('required'),
+              opponentScore: Yup.string()
+                .required('required'),
+            })}
           >
+            {props => {
+              const {
+                handleSubmit,
+                isSubmitting,                 
+              } = props
+              return (
+                <form
+                  onSubmit={handleSubmit}
+                  style={{
+                    marginTop: 50,
+                    width: '300px',
+                    display: 'flex', 
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                  }}
+                >
 
-            <FormControl>
-              <SelectOpponent 
-                style={{
-                  marginTop: 50,
-                }}
-              />
-            </FormControl>
+                  <FormControl variant='outlined'>
+                    <SelectOpponent 
+                      style={{
+                        marginTop: 50,
+                      }}
+                    />
+                  </FormControl>
 
-            <div
-              style={{
-                marginTop: 50,
-                display: 'flex', 
-                justifyContent: 'space-between',
-              }}
-            >
-              <TextField
-                id='standard-number'
-                label='Your Score'
-                // value={}
-                // onChange={}
-                type='number'
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                style={{
-                  width: '45%',
-                }}
-              />
+                  <div
+                    style={{
+                      marginTop: 50,
+                      display: 'flex', 
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <TextField
+                      id='standard-number'
+                      label='Your Score'
+                      // value={}
+                      // onChange={}
+                      type='number'
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      style={{
+                        width: '45%',
+                      }}
+                      variant='outlined'
+                    />
 
-              <TextField
-                id='standard-number'
-                label='Opponent Score'
-                // value={}
-                // onChange={}
-                type='number'
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                style={{
-                  width: '45%',
-                }}
-              />
-            </div>
+                    <TextField
+                      id='standard-number'
+                      label='Opponent Score'
+                      // value={}
+                      // onChange={}
+                      type='number'
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      style={{
+                        width: '45%',
+                      }}
+                      variant='outlined'
+                    />
+                  </div>
 
-            <Link style={{ marginTop: 50, width: '75%', textDecoration: 'none' }} to='/home'>
-              <Button 
-                type='submit'
-                color='primary'
-                disabled={isSubmitting} 
-                variant='contained' 
-                style={{
-                  width: '100%',
-                }}
-              >
-                Submit Scores
-              </Button>
-            </Link>
-     
-          </form>
-        )
-      }}
-    </Formik>
+                  <Link style={{ marginTop: 50, width: '75%', textDecoration: 'none' }} to='/home'>
+                    <Button 
+                      type='submit'
+                      color='secondary'
+                      disabled={isSubmitting} 
+                      variant='contained' 
+                      style={{
+                        width: '100%',
+                      }}
+                    >
+                      Submit Scores
+                    </Button>
+                  </Link>
+          
+                </form>
+              )
+            }}
+          </Formik>
+      {/* )}}
+      </Mutation> */}
   </div>
 )
 
