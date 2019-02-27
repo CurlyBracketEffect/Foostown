7b01b992addc82425afc94b3fd1bffd83db196a5
VPaulger <vaughn_paulger@live.ca>
2019-02-26 20:33:54 -0500

    feat: created resolver for teams query

--- packages/server/query/queryResolvers.js
+++ packages/server/query/queryResolvers.js
@@ -3,5 +3,27 @@ module.exports = {
     async user(parent, args, { req, app }) {
       return 'Success'
     },
+
+    async viewer(parent, args, { req, app, postgres }) {
+      const userID = authenticate(app, req)
+
+      const findUserQuery = {
+        text: 'SELECT * FROM foostown.users WHERE id = $1',
+        values: [args.input.id],
+      }
+
+      const user = await postgres.query(findUserQuery)
+      return user.rows[0]
+    },
+
+    async teams (parent, args, { req, app, postgres }) {
+      const userID = authenticate(app, req)
+
+      const teams = await postgres.query({
+        text: 'SELECT * FROM foostown.teams',
+        values: [userID],
+      })
+      return teams.rows
+    },
   },
 }
