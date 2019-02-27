9209872a39d5fcf610c727736cd670604890f7e6
Kayahs <akshaykmanchanda@gmail.com>
2019-02-27 10:19:57 -0500

    FIX: fixing folder name issue pt 1

--- packages/client/src/playGame/UsersAndStats.js
+++ packages/client/src/playGame/UsersAndStats.js
@@ -17,18 +17,18 @@ import {
 const UsersAndStats = () => (
   <Query
     query={gql`
-    query{
-      organization(id: 1){
-        users{
-          fullname
-          stats{
-            matches_played
-            goals_for
-            goals_against
+      query {
+        organization(id: 1) {
+          users {
+            fullname
+            stats {
+              matches_played
+              goals_for
+              goals_against
+            }
           }
         }
       }
-    }
     `}
   >
     {({ loading, error, data }) => {
@@ -41,11 +41,15 @@ const UsersAndStats = () => (
           <ListItem button style={{ height: '75px' }}>
             {/* {console.log(data)} */}
             <ListItemIcon>Avatar</ListItemIcon>
-            <ListItemText primary={user.fullname.length>13 ? user.fullname.slice(0,13) + '...' : user.fullname} />
+            <ListItemText
+              primary={
+                user.fullname.length > 13 ? user.fullname.slice(0, 13) + '...' : user.fullname
+              }
+            />
             <ListItemSecondaryAction>
-              G.P.: {user.stats.matches_played} <br/>
-              G.F.: {user.stats.goals_for} <br/>
-              G.A.: {user.stats.goals_against} <br/>
+              G.P.: {user.stats.matches_played} <br />
+              G.F.: {user.stats.goals_for} <br />
+              G.A.: {user.stats.goals_against} <br />
             </ListItemSecondaryAction>
           </ListItem>
         )
@@ -55,5 +59,3 @@ const UsersAndStats = () => (
 )
 
 export default UsersAndStats
-
-
