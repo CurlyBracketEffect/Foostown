f26fa0088f0a3a6fb50b21bb8f8d031aaa069704
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2019-02-28 17:16:02 -0500

    fix: fixed a bug that was making the app crash when visiting the create game page.

--- packages/client/src/Play-Game/SelectOpponent.js
+++ packages/client/src/Play-Game/SelectOpponent.js
@@ -41,7 +41,7 @@ const SelectOpponent = ({ value, onChange }) => {
                 input={<OutlinedInput />}
               >
 
-              {/* {console.log(data.viewer)} */}
+              {console.log(data.viewer)}
                 {data.viewer.teams.map((team) => (
                   <MenuItem key={team.id} value={parseInt(team.id)}>
                  { console.log(team.id)}
