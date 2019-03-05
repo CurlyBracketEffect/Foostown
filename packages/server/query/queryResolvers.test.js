8e704fd080490a41219f25429d2c69fe92ec53e6
Eirian-Ta <trang.ta911@gmail.com>
2019-03-05 17:59:11 -0500

    fix: testing query teams

--- packages/server/query/queryResolvers.test.js
+++ packages/server/query/queryResolvers.test.js
@@ -1,9 +1,20 @@
 const { Query } = require('./queryResolvers')
 const postgres = require('../postgres')
 
+const app = {
+  get: (key) => {
+    if (key === 'SKIP_AUTH') {
+      return true
+    }
+    return null
+  }
+}
+
+
 describe("Query Resolvers", () => {
   describe(Query.teams, () => {
     // integration test
+
     describe('when a team exists for another organization', () => {
       it('does not return that team', async () => {
         const userRows = await postgres.query({
@@ -23,7 +34,7 @@ describe("Query Resolvers", () => {
           values: ['the giraffes', organizationID]
         })
 
-        const result = await Query.teams(null, { organizationID: organizationID + 1 }, { postgres })
+        const result = await Query.teams(null, { organizationID: organizationID + 1 }, { app, postgres })
 
         expect(result).toEqual([])
       })
@@ -47,7 +58,7 @@ describe("Query Resolvers", () => {
         values: ['the giraffes', organizationID]
       })
 
-      const result = await Query.teams(null, { organizationID }, { postgres })
+      const result = await Query.teams(null, { organizationID }, { postgres, app })
 
       expect(result.length).toEqual(1)
       expect(result[0].team_name).toEqual('the giraffes')
